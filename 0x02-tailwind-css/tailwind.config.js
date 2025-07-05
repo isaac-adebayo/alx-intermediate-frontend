c46546b0e12877f558fb400f44a509dae2347ae7
Isaac Adebayo <isaacadebayo.ai@gmail.com>
2025-07-06 00:31:04 +0100

    add new files and directory

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// This file is used to configure Tailwind CSS.
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
